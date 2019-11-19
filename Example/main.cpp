25e55ae1e1acfc425d31c917e8a50d20b1dc647c
Centimo <mohenti@gmail.com>
2019-11-19 07:50:03 +0300

    Update Example

--- Example/main.cpp
+++ Example/main.cpp
@@ -6,24 +6,31 @@
 
 int main()
 {
+
+  if constexpr (false)
   {
     std::random_device rd;
     std::mt19937 generator(rd());
     std::uniform_real_distribution<double> uniform_distribution(-1000.0, 1000.0);
 
     std::ofstream output_stream("data.txt", std::ios::out);
-    for (int i = 0; i < 1500; ++i)
+    for (int i = 0; i < 40000; ++i)
     {
-      for (int j = 0; j < 39; ++j)
+      for (int j = 0; j < 31; ++j)
       {
-        output_stream << uniform_distribution(generator) << ", ";
+        output_stream << uniform_distribution(generator) << " ";
       }
 
       output_stream << uniform_distribution(generator) << "\n";
+      if (i % 10000 == 0)
+      {
+        std::cout << "10k points: " << i << std::endl;
+      }
     }
   }
 
 
+  auto start_time = std::chrono::steady_clock::now();
   auto processor = K_means_lib::make_k_means_processor("config.txt");
   if (!processor)
   {
@@ -31,8 +38,15 @@ int main()
     return 1;
   }
 
+  std::cout << "File reading time (ms): "
+    << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time).count() << std::endl;
+
+  start_time = std::chrono::steady_clock::now();
   processor->start();
-  K_means_lib::print_result_to_file("out.txt", processor->get_result());
+  auto result = processor->get_result();
+  std::cout << "Processing time (ms): "
+    << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time).count() << std::endl;
+  K_means_lib::print_result_to_file("out.txt", result);
 
   return 0;
 }
